5837e01fe237abdbd9cc37d52cac8cd8c060d6c4
Charis-Opol <charisopol@gmail.com>
2025-09-12 15:12:27 +0300

    Class work

---
+++ Name.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Name{
+    public static void main(String[] args) {
+        System.out.println("Enter your first name: ");
+        Scanner Scanner = new Scanner(System.in);
+        String firstName = Scanner.nextLine();
+        System.out.println("Enter your surname: ");
+        String surname = Scanner.nextLine();
+        System.out.println("Enter your registration number: ");
+        String regnum = Scanner.nextLine();
+
+        System.out.println("Student Name: " + firstName + " " + surname);
+        System.out.println("Student registration number: " + regnum);
+    }
+}
--- printing.java
+++ printing.java
@@ -4,6 +4,8 @@ public class printing {
         System.out.println("I am learning Java.");
         System.out.println("It is awesome!");
         System.out.println("This sentence will work!");
+        System.out.print("OOP is amazing! ");
+        System.out.print("Especially in Java!");
     }
 }
 
