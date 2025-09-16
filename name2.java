777eee10abac1f07a303c95c2c8cc967317b64b5
Charis-Opol <charisopol@gmail.com>
2025-09-16 17:52:07 +0300

    Assignment

--- name2.java
+++ name2.java
@@ -1,7 +1,7 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Name{
-    public static void main(String[] args) {
+public class name2 {
+        public static void main(String[] args) {
         System.out.println("Enter your first name: ");
         Scanner Scanner = new Scanner(System.in);
         String firstName = Scanner.nextLine();
